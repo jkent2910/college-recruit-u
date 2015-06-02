135980f99c538c42e76b5a494e7a67c8d2477fca
Julie Kent <jkent2910@gmail.com>
2015-06-01 20:33:43 -0400

    twitter sign-in functionality added.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,23 @@
 class User < ActiveRecord::Base
   # Others available are: :timeoutable and :omniauthable
+
   devise :database_authenticatable, :confirmable, :lockable, :registerable, :recoverable,
-         :rememberable, :trackable, :validatable
+         :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:twitter]
 
   has_one :student_profile
+
+  def self.from_omniauth(auth)
+  	where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+  		if user.provider == "twitter"
+  			user.email = "#{self.name}_email@example.com"
+  		else
+  			user.email = auth.info.email
+  		end
+  		user.provider = auth.provider
+  		user.uid = auth.uid
+  		user.password = Devise.friendly_token[0,20]
+  		user.skip_confirmation!
+  	end
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :student_profiles
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks"}
 
   root 'welcome#index'
 
