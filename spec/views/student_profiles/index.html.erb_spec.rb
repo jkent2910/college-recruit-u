1bdcf1b2adc5e0718d192bad631d5468068d210b
David W. Body <davidbody@bigcreek.com>
2015-06-15 13:44:29 -0500

    Converted to Ruby 1.9 hash syntax

--- spec/views/student_profiles/index.html.erb_spec.rb
+++ spec/views/student_profiles/index.html.erb_spec.rb
@@ -10,28 +10,28 @@ RSpec.describe "student_profiles/index", type: :view do
 
     assign(:student_profiles, [
       StudentProfile.create!(
-        :first_name => "First Name",
-        :last_name => "Last Name",
-        :gender => "Gender",
-        :high_school => "High School",
-        :graduation_year => "Graduation Year"
+        first_name: "First Name",
+        last_name: "Last Name",
+        gender: "Gender",
+        high_school: "High School",
+        graduation_year: "Graduation Year"
       ),
       StudentProfile.create!(
-        :first_name => "First Name",
-        :last_name => "Last Name",
-        :gender => "Gender",
-        :high_school => "High School",
-        :graduation_year => "Graduation Year"
+        first_name: "First Name",
+        last_name: "Last Name",
+        gender: "Gender",
+        high_school: "High School",
+        graduation_year: "Graduation Year"
       )
     ])
   end
 
   it "renders a list of student_profiles" do
     render
-    assert_select "tr>td", :text => "First Name".to_s, :count => 2
-    assert_select "tr>td", :text => "Last Name".to_s, :count => 2
-    assert_select "tr>td", :text => "Gender".to_s, :count => 2
-    assert_select "tr>td", :text => "High School".to_s, :count => 2
-    assert_select "tr>td", :text => "Graduation Year".to_s, :count => 2
+    assert_select "tr>td", text: "First Name".to_s, count: 2
+    assert_select "tr>td", text: "Last Name".to_s, count: 2
+    assert_select "tr>td", text: "Gender".to_s, count: 2
+    assert_select "tr>td", text: "High School".to_s, count: 2
+    assert_select "tr>td", text: "Graduation Year".to_s, count: 2
   end
 end
