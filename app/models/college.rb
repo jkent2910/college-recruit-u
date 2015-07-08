f6375cdb2b5b3f2569aec6029890a920f5128f82
Julie Kent <jkent2910@gmail.com>
2015-07-08 14:08:22 -0500

    added validations for college model

--- app/models/college.rb
+++ app/models/college.rb
@@ -1,2 +1,5 @@
 class College < ActiveRecord::Base
+  
+  validates_presence_of :name, :type, :city, :state, :region
+
 end
--- spec/models/college_spec.rb
+++ spec/models/college_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe College, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validations" do
+    [:name, :type, :city, :state, :region].each do |attr|
+      it "validates presence of #{attr}" do
+        expect(subject).to validate_presence_of(attr)
+      end
+    end
+  end
 end
