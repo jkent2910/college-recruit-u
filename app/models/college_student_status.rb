dfaf96afc9840ac286a0a9c25c3d6e22228e57b5
David W. Body <davidbody@bigcreek.com>
2015-07-13 13:51:48 -0500

    Validate status name

--- app/models/college_student_status.rb
+++ app/models/college_student_status.rb
@@ -2,7 +2,8 @@ class CollegeStudentStatus < ActiveRecord::Base
 
   VALID_STATUSES = ['Just a Fan', 'Considering', 'Applying', 'Enrolling']
 
+  validates :status_name, inclusion: { in: VALID_STATUSES }
+
   belongs_to :student_profile
   belongs_to :college
-
 end
--- spec/models/college_student_status_spec.rb
+++ spec/models/college_student_status_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe CollegeStudentStatus, type: :model do
+  it "validates status" do
+    expect(subject).to validate_inclusion_of(:status_name).in_array(CollegeStudentStatus::VALID_STATUSES)
+  end
+
   it "belongs to student profile" do
     expect(subject).to belong_to(:student_profile)
   end
@@ -8,5 +12,4 @@ RSpec.describe CollegeStudentStatus, type: :model do
   it "belongs to college" do
     expect(subject).to belong_to(:college)
   end
-
 end
