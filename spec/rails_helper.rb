fbe0e74661d295cf61ffe3a189e3c0f49784d940
David W. Body <davidbody@bigcreek.com>
2015-07-23 13:59:05 -0500

    Added DatabaseCleaner configuration (how did things work before?)

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -51,4 +51,15 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
   config.include Paperclip::Shoulda::Matchers
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
 end
