251c119cd1edc271cc185ab21b9609cd6883e546
David W. Body <davidbody@bigcreek.com>
2015-08-13 12:33:27 -0500

    Fixed quotes

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,4 +1,3 @@
-# coding: utf-8
-run “ln -nfs #{config.shared_path}/config/google_college_credentials.json #{config.release_path}/config/google_college_credentials.json”
-run “ln -nfs #{config.shared_path}/config/s3.yml #{config.release_path}/config/s3.yml”
-run “ln -nfs #{config.shared_path}/config/secrets.yml #{config.release_path}/config/secrets.yml”
+run ln -nfs "#{config.shared_path}/config/google_college_credentials.json #{config.release_path}/config/google_college_credentials.json"
+run ln -nfs "#{config.shared_path}/config/s3.yml #{config.release_path}/config/s3.yml"
+run ln -nfs "#{config.shared_path}/config/secrets.yml #{config.release_path}/config/secrets.yml"
