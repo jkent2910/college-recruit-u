9ce493c138cff06c5fb0a1707ea05b32457690f1
David W. Body <davidbody@bigcreek.com>
2015-08-22 15:35:43 -0500

    Default url options for action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,8 +65,8 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # For generating links in emails.
-  # TODO: Update this for production.
-  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # TODO: Update this with the actual host name.
+  config.action_mailer.default_url_options = { host: 'ec2-52-6-229-150.compute-1.amazonaws.com' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
