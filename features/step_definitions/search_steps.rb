35da4018b76472c460b372d530282ae064e3f584
Julie Kent <jkent2910@gmail.com>
2015-08-28 19:41:17 -0500

    fixed failing cucumber test

--- features/step_definitions/search_steps.rb
+++ features/step_definitions/search_steps.rb
@@ -6,14 +6,14 @@ When(/^I search for "(.*?)"$/) do |query|
 end
 
 Then(/^I should see the following colleges$/) do |table|
-  college_results = find("#college-search-results ul")
+  college_results = find("#college-search-results")
   table.hashes.each do |college|
     expect(college_results).to have_text(college[:name])
   end
 end
 
 Then(/^I should see the following students$/) do |table|
-  student_results = find("#student-search-results ul")
+  student_results = find("#student-search-results")
   table.hashes.each do |student|
     expect(student_results).to have_text(student[:name])
   end
