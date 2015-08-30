e256cab1bf24c47465fcbd1bcad6794effed1264
David W. Body <davidbody@bigcreek.com>
2015-08-30 17:05:41 -0500

    Hard-coding production in production.rb is OK, right?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
 
   # For generating links in emails.
   # TODO: Update this with the actual host name.
-  sendgrid_config = YAML.load_file("#{Rails.root}/config/sendgrid.yml")[RAILS_ENV]
+  sendgrid_config = YAML.load_file("#{Rails.root}/config/sendgrid.yml")['production']
 
   config.action_mailer.default_url_options = { host: 'ec2-52-6-207-202.compute-1.amazonaws.com' }
 
