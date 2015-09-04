468c64cd20c0a2333dbcc2e85edc8477e4419d25
David W. Body <davidbody@bigcreek.com>
2015-09-04 15:15:56 -0500

    Stop confirming user email addresses

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  # Others available are: :timeoutable and :omniauthable
-  devise :database_authenticatable, :confirmable, :lockable, :registerable, :recoverable,
+  # Others available are: :confirmable, :timeoutable, and :omniauthable
+  devise :database_authenticatable, :lockable, :registerable, :recoverable,
          :rememberable, :trackable, :validatable
 
   has_one :student_profile
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150822002304) do
+ActiveRecord::Schema.define(version: 20150904200937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -121,8 +121,8 @@ ActiveRecord::Schema.define(version: 20150822002304) do
     t.string   "gender"
     t.string   "high_school"
     t.string   "graduation_year"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
     t.string   "photo_file_name"
     t.string   "photo_content_type"
     t.integer  "photo_file_size"
@@ -131,6 +131,7 @@ ActiveRecord::Schema.define(version: 20150822002304) do
     t.text     "majors"
     t.string   "slug"
     t.string   "cell_number"
+    t.boolean  "incognito",          default: false
   end
 
   add_index "student_profiles", ["slug"], name: "index_student_profiles_on_slug", unique: true, using: :btree
@@ -146,10 +147,6 @@ ActiveRecord::Schema.define(version: 20150822002304) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
     t.integer  "failed_attempts",        default: 0,  null: false
     t.string   "unlock_token"
     t.datetime "locked_at"
@@ -159,7 +156,6 @@ ActiveRecord::Schema.define(version: 20150822002304) do
     t.string   "uid"
   end
 
-  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
