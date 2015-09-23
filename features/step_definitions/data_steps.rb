be3e7bae7896e59a29407363c402a273f7d70a10
David W. Body <davidbody@bigcreek.com>
2015-09-23 15:57:28 -0500

    Starting work on displaying colleges by popularity

--- features/step_definitions/data_steps.rb
+++ features/step_definitions/data_steps.rb
@@ -10,8 +10,18 @@ Given(/^the following colleges?:$/) do |table|
   end
 end
 
-Given(/^the following student profiles:$/) do |table|
+Given(/^the following student profiles?:$/) do |table|
   table.hashes.each do |hash|
     FactoryGirl.create(:student_profile, hash)
   end
+end
+
+Then(/^I should see the following colleges?:$/) do |table|
+  # table is a Cucumber::Ast::Table
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the colleges have the following student interest?:$/) do |table|
+  # table is a Cucumber::Ast::Table
+  pending # express the regexp above with the code you wish you had
 end
