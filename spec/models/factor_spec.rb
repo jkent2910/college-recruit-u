ea502d8b68bc0641069679eca2ac624885d8fd9b
David W. Body <davidbody@bigcreek.com>
2015-09-30 16:51:54 -0500

    Hard coded progress factors

--- spec/models/factor_spec.rb
+++ spec/models/factor_spec.rb
@@ -8,4 +8,16 @@ RSpec.describe Factor, type: :model do
   it "has many choices" do
     expect(subject).to have_many(:factor_choices)
   end
+
+  [3, 4, 7, 8].each do |n|
+    specify "factor with id of #{n} is a progress factor" do
+      expect(Factor.new(id: n)).to be_progress_factor
+    end
+  end
+
+  [1, 2, 5, 6].each do |n|
+    specify "factor with id of #{n} is not a progress factor" do
+      expect(Factor.new(id: n)).not_to be_progress_factor
+    end
+  end
 end
